380fa2d45679ea8f59a21a261e5c4493e2a0e29a
HildebrandoLima <hildebrandolima16@gmail.com>
2023-04-12 17:42:42 -0300

    Listagem de usuários

--- app/Infra/Database/Dao/User/ListUserDb.php
+++ app/Infra/Database/Dao/User/ListUserDb.php
@@ -10,73 +10,38 @@ class ListUserDb extends DbBase
 {
     public function listUserAll(): Collection
     {
-        return collect($this->db
-        ->table('users as u')
-        ->leftJoin('endereco as e', 'e.usuario_id', '=', 'u.id')
-        ->leftJoin('unidade_federativa as uf', 'uf.id', '=', 'e.uf_id')
-        ->leftJoin('telefone as t', 't.usuario_id', '=', 'u.id')
-        ->leftJoin('ddd as d', 'd.id', '=', 't.ddd_id')
+        return $this->db
+        ->table('users')
         ->select([
-            'u.id as usuarioId',
-            'u.name as nome',
-            'u.ativo as atividade',
-            'u.genero as genero',
-            'e.id as enderecoId',
-            'e.logradouro as logradouro',
-            'e.descricao as descricao',
-            'e.bairro as bairro',
-            'e.cidade as cidade',
-            'e.cep as cep',
-            'uf.id as ufId',
-            'uf.uf as uf',
-            'uf.descricao as descricaoUF',
-            't.numero as numero',
-            't.tipo as tipo',
-            'd.id as dddId',
-            'd.ddd as ddd',
-            'd.descricao as descricaoDDD',
-            'u.created_at as criadoEm',
-            'u.updated_at as alteradoEm'
+            'id as usuarioId',
+            'name as nome',
+            'ativo as atividade',
+            'genero as genero',
+            'created_at as criadoEm',
+            'updated_at as alteradoEm'
         ])
-        ->orderBy('u.id')->paginate(10));
+        ->orderBy('id')
+        ->get();
     }
 
     public function listUserFind(UserRequest $request): Collection
     {
         $query = $this->db
         ->table('users as u')
-        ->leftJoin('endereco as e', 'e.usuario_id', '=', 'u.id')
-        ->leftJoin('unidade_federativa as uf', 'uf.id', '=', 'e.uf_id')
-        ->leftJoin('telefone as t', 't.usuario_id', '=', 'u.id')
-        ->leftJoin('ddd as d', 'd.id', '=', 't.ddd_id')
         ->select([
             'u.id as usuarioId',
             'u.name as nome',
             'u.ativo as atividade',
             'u.genero as genero',
-            'e.id as enderecoId',
-            'e.logradouro as logradouro',
-            'e.descricao as descricao',
-            'e.bairro as bairro',
-            'e.cidade as cidade',
-            'e.cep as cep',
-            'uf.id as ufId',
-            'uf.uf as uf',
-            'uf.descricao as descricaoUF',
-            't.numero as numero',
-            't.tipo as tipo',
-            'd.id as dddId',
-            'd.ddd as ddd',
-            'd.descricao as descricaoDDD',
             'u.created_at as criadoEm',
             'u.updated_at as alteradoEm'
         ]);
         if (isset($request->usuarioId)):
-            $query->where('u.id', $request->usuarioId);
+            $query->where('id', $request->usuarioId);
         endif;
 
         if (isset($request->usuarioNome)):
-            $query->where('u.name', 'like', '%' . $request->usuarioNome . '%');
+            $query->where('name', 'like', '%' . $request->usuarioNome . '%');
         endif;
         return $query->get();
     }
