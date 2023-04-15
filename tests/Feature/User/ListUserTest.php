deac0b7541b1eeff1239fc972dd12cdf1f993a53
HildebrandoLima <hildebrandolima16@gmail.com>
2023-04-15 15:15:28 -0300

    TDD listagem de usuário(s)

---
+++ tests/Feature/User/ListUserTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature\User;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ListUserTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_endpoint_get_all_a_successful_response(): void
+    {
+        $response = $this->getJson('/api/user/list', []);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_get_single_a_successful_response(): void
+    {
+        $response = $this->getJson('/api/user/list/', [1]);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_get_all_a_failure_response(): void
+    {
+        $response = $this->getJson('/api/user/list', []);
+        $response->assertStatus( 401);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_get_single_a_failure_response(): void
+    {
+        $response = $this->getJson('/api/user/list', [1]);
+        $response->assertStatus( 401);
+    }
+}
