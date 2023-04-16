9ca37c7be5ab141aa1599601e9798ae54c41f67f
HildebrandoLima <hildebrandolima16@gmail.com>
2023-04-16 09:34:58 -0300

    TDD listagem de uf e endereço

---
+++ tests/Feature/Anddress/ListAddressTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace Tests\Feature\Address;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ListAddressTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_endpoint_get_uf_a_successful_response(): void
+    {
+        $response = $this->getJson('/api/address/uf/list');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_get_address_a_successful_response(): void
+    {
+        $response = $this->getJson('/api/address/list/1');
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_get_uf_a_failure_response(): void
+    {
+        $response = $this->getJson('/api/address/uf/list');
+        $response->assertStatus( 401);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_get_address_a_failure_response(): void
+    {
+        $response = $this->getJson('/api/address/list/');
+        $response->assertStatus( 200);
+    }
+}
