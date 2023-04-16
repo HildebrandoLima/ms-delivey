5968859c961dae34fd69aee633b1b5207fcd1301
HildebrandoLima <hildebrandolima16@gmail.com>
2023-04-16 09:02:36 -0300

    TDD edição de fornecedor

---
+++ tests/Feature/Provider/EditProviderTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature\Provider;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class EditProviderTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_endpoint_put_create_a_successful_response(): void
+    {
+        $provider = [
+            'fornecedorId'=> 1,
+            'nome' => 'Teste',
+            'cnpj' => rand(1000, 10000),
+            'email' => 'email@email.com.br',
+            'ativo' => '1',
+            'data_fundacao' => new \dateTime(),
+        ];
+        $response = $this->putJson('/api/provider/edit', $provider);
+        $response->assertStatus(200);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_put_create_a_failure_response(): void
+    {
+        $provider = [
+            'fornecedorId'=> 1,
+            'nome' => 'Teste',
+            'cnpj' => rand(1000, 10000),
+            'email' => '',
+            'ativo' => '',
+            'data_fundacao' => new \dateTime(),
+        ];
+        $response = $this->putJson('/api/provider/edit', $provider);
+        $response->assertStatus(422);
+    }
+}
