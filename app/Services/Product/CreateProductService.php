b00a65533a85ec46c6ee033cb396e9a5ed2d0ccc
HildebrandoLima <hildebrandolima16@gmail.com>
2023-05-22 21:03:53 -0300

    Crud de produto

--- app/Services/Product/CreateProductService.php
+++ app/Services/Product/CreateProductService.php
@@ -3,30 +3,32 @@
 namespace App\Services\Product;
 
 use App\Http\Requests\ProductRequest;
+use App\Models\Produto;
 use App\Repositories\ProductRepository;
 use App\Services\Product\Interfaces\ICreateProductService;
+use App\Support\Utils\Cases\ProductCase;
 use App\Support\Utils\CheckRegister\CheckProduct;
 use App\Support\Utils\CheckRegister\CheckProvider;
-use App\Support\Utils\MapToModel\ProductModel;
+use App\Support\Utils\Enums\ProductEnums;
 
 class CreateProductService implements ICreateProductService
 {
     private CheckProduct $checkProduct;
     private CheckProvider $checkProvider;
-    private ProductModel $productModel;
+    private ProductCase $productCase;
     private ProductRepository $productRepository;
 
     public function __construct
     (
         CheckProduct      $checkProduct,
         CheckProvider     $checkProvider,
-        ProductModel      $productModel,
+        ProductCase       $productCase,
         ProductRepository $productRepository
     )
     {
         $this->checkProduct      = $checkProduct;
         $this->checkProvider     = $checkProvider;
-        $this->productModel      = $productModel;
+        $this->productCase       = $productCase;
         $this->productRepository = $productRepository;
     }
 
@@ -34,7 +36,25 @@ class CreateProductService implements ICreateProductService
     {
         $this->checkProduct->checkProductExist($request);
         $this->checkProvider->checkProviderIdExist($request->fornecedorId);
-        $product = $this->productModel->productModel($request, 'create');
+        $product = $this->mapToModel($request);
         return $this->productRepository->insert($product);
+    }
+
+    private function mapToModel(ProductRequest $request): Produto
+    {
+        $product = new Produto();
+        $product->nome = $request->nome;
+        $product->preco_custo = $request->precoCusto;
+        $product->margem_lucro = ($request->precoVenda - $request->precoCusto);
+        $product->preco_venda = $request->precoVenda;
+        $product->codigo_barra = $request->codigoBarra;
+        $product->descricao = $request->descricao;
+        $product->quantidade = $request->quantidade;
+        $product->unidade_medida = $this->productCase->productCase($request->unidadeMedida);
+        $product->data_validade = $request->dataValidade;
+        $product->categoria_id = $request->categoriaId;
+        $product->fornecedor_id = $request->fornecedorId;
+        $product->ativo = ProductEnums::ATIVADO;
+        return $product;
     }
 }
