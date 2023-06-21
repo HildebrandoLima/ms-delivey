50e4b190096a2dc6dcca586106e1d63dfd0cf9d8
HildebrandoLima <hildebrandolima16@gmail.com>
2023-06-21 17:49:41 -0300

    Repository, injeção de dependências e dto para entidade de telefone

--- app/DataTransferObjects/RequestsDtos/TelephoneRequestDto.php
+++ app/DataTransferObjects/RequestsDtos/TelephoneRequestDto.php
@@ -3,14 +3,16 @@
 namespace App\DataTransferObjects\RequestsDtos;
 
 use App\DataTransferObjects\Dtos\TelephoneDto;
+use App\Support\Utils\Cases\TelephoneCase;
 
 class TelephoneRequestDto
 {
     public static function fromRquest(array $telefone): TelephoneDto
     {
         $telephoneDto = new TelephoneDto();
+        $type = new TelephoneCase();
         $telephoneDto->setNumero($telefone['numero']);
-        $telephoneDto->setTipo($telefone['tipo']);
+        $telephoneDto->setTipo($type->typeCase($telefone['tipo']));
         $telephoneDto->setDddId($telefone['dddId']);
         $telephoneDto->setUsuarioId($telefone['usuarioId'] ?? null);
         $telephoneDto->setFornecedorId($telefone['fornecedorId'] ?? null);
--- app/Services/Telephone/EditTelephoneService.php
+++ app/Services/Telephone/EditTelephoneService.php
@@ -2,51 +2,34 @@
 
 namespace App\Services\Telephone;
 
+use App\DataTransferObjects\RequestsDtos\TelephoneRequestDto;
 use App\Http\Requests\TelephoneRequest;
-use App\Models\Telefone;
-use App\Repositories\CheckRegisterRepository;
 use App\Repositories\Concretes\TelephoneRepository;
+use App\Repositories\Interfaces\CheckEntityRepositoryInterface;
 use App\Services\Telephone\Interfaces\IEditTelephoneService;
-use App\Support\Utils\Cases\TelephoneCase;
-use App\Support\Utils\Enums\TelephoneEnum;
 
 class EditTelephoneService implements IEditTelephoneService
 {
-    private TelephoneCase $telephoneCase;
-    private CheckRegisterRepository $checkRegisterRepository;
-    private TelephoneRepository $telephoneRepository;
+    private CheckEntityRepositoryInterface $checkEntityRepositoryInterface;
+    private TelephoneRepository            $telephoneRepository;
 
     public function __construct
     (
-        TelephoneCase           $telephoneCase,
-        CheckRegisterRepository $checkRegisterRepository,
-        TelephoneRepository     $telephoneRepository
+        CheckEntityRepositoryInterface $checkEntityRepositoryInterface,
+        TelephoneRepository            $telephoneRepository
     )
     {
-        $this->telephoneCase           = $telephoneCase;
-        $this->checkRegisterRepository = $checkRegisterRepository;
-        $this->telephoneRepository     = $telephoneRepository;
+        $this->checkEntityRepositoryInterface = $checkEntityRepositoryInterface;
+        $this->telephoneRepository            = $telephoneRepository;
     }
 
     public function editTelephone(int $id, TelephoneRequest $request): bool
     {
         foreach ($request->telefones as $telefone):
-            $this->checkRegisterRepository->checkTelephoneIdExist($id);
-            $telephone = $this->mapToModel($telefone);
+            $this->checkEntityRepositoryInterface->checkTelephoneIdExist($id);
+            $telephone = TelephoneRequestDto::fromRquest($telefone);
             $this->telephoneRepository->update($id, $telephone);
         endforeach;
         return true;
-    }
-
-    private function mapToModel(array $telephones): Telefone
-    {
-        $telephone = new Telefone();
-        $telephone->numero = str_replace('-', "", $telephones['numero']);
-        $telephone->tipo = $this->telephoneCase->typeCase($telephones['tipo']);
-        $telephone->ddd_id = $telephones['dddId'];
-        $telephone->usuario_id = $telephones['usuarioId'] ?? null;
-        $telephone->fornecedor_id = $telephones['fornecedorId'] ?? null;
-        $telephones['ativo'] == 1 ? $telephone->ativo = TelephoneEnum::ATIVADO : $telephone->ativo = TelephoneEnum::DESATIVADO;
-        return $telephone;
     }
 }
