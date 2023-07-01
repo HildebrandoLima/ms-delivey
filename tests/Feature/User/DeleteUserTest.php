40b360e64b23f7250d05b7eb682ddbc36824df07
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-01 17:55:13 -0300

    Habiliytar/Desabilitar usuário, endereço e telefone

--- tests/Feature/User/DeleteUserTest.php
+++ tests/Feature/User/DeleteUserTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Feature\User;
 
+use App\Models\Endereco;
+use App\Models\Telefone;
 use App\Models\User;
+use App\Support\Utils\Enums\PerfilEnum;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -11,19 +14,45 @@ class DeleteUserTest extends TestCase
     /**
      * @test
      */
-    public function it_endpoint_delete_remove_a_failure_response(): void
+    public function it_endpoint_enable_disable_base_response_200(): void
     {
-        //
+        $data = User::factory()->createOne()->toArray();
+        Endereco::factory()->createOne(['usuario_id' => $data['id'], 'fornecedor_id' => null])->toArray();
+        Telefone::factory()->createOne(['usuario_id' => $data['id'], 'fornecedor_id' => null])->toArray();
+
+        $authenticate = $this->authenticate(PerfilEnum::ADMIN);
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $authenticate['accessToken'],
+        ])->putJson(route('user.enable.disable', ['id' => base64_encode($data['id']), 'active' => 0]));
+        $this->assertEquals($this->httpStatusCode($response), 200);
     }
 
     /**
      * @test
      */
-    public function it_endpoint_delete_remove_a_successful_response(): void
+    public function it_endpoint_enable_disable_base_response_400(): void
     {
-        $user = User::factory()->createOne();
-        $data = $user->toArray();
-        $response = $this->deleteJson(route('user.remove', ['id' => base64_encode($data['id'])]));
-        $response->assertStatus(200);
+        $data = User::factory()->createOne()->toArray();
+        Endereco::factory()->createOne(['usuario_id' => $data['id'], 'fornecedor_id' => null])->toArray();
+        Telefone::factory()->createOne(['usuario_id' => $data['id'], 'fornecedor_id' => null])->toArray();
+
+        $authenticate = $this->authenticate(PerfilEnum::ADMIN);
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $authenticate['accessToken'],
+        ])->putJson(route('user.enable.disable', ['id' => base64_encode($data['id']), 'active' => 1]));
+        $this->assertEquals($this->httpStatusCode($response), 400);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_enable_disable_base_response_401(): void
+    {
+        $data = User::factory()->createOne()->toArray();
+        Endereco::factory()->createOne(['usuario_id' => $data['id'], 'fornecedor_id' => null])->toArray();
+        Telefone::factory()->createOne(['usuario_id' => $data['id'], 'fornecedor_id' => null])->toArray();
+
+        $response = $this->putJson(route('user.enable.disable', ['id' => base64_encode($data['id']), 'active' => 0]));
+        $this->assertEquals($this->httpStatusCode($response), 401);
     }
 }
