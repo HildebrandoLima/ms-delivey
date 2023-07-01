dd6b8dad2f4b34f5fd45444ff06d5e78892ca642
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-01 18:03:10 -0300

    Testes de login

--- tests/Feature/Auth/LoginTest.php
+++ tests/Feature/Auth/LoginTest.php
@@ -17,6 +17,32 @@ class LoginTest extends TestCase
             'password' => 'HiLd3br@ndo'
         ];
         $response = $this->postJson(route('auth.login'), $data);
-        $response->assertStatus(200);
+        $this->assertEquals($this->httpStatusCode($response), 200);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_login_a_successful_response_email_invalid(): void
+    {
+        $data = [
+            'email' => 'hildebrandolima16@gmail',
+            'password' => 'HiLd3br@ndo'
+        ];
+        $response = $this->postJson(route('auth.login'), $data);
+        $this->assertEquals($this->httpStatusCode($response), 400);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_login_a_successful_response_password_invalid(): void
+    {
+        $data = [
+            'email' => 'hildebrandolima16@gmail.com',
+            'password' => 'HiLd3brndo'
+        ];
+        $response = $this->postJson(route('auth.login'), $data);
+        $this->assertEquals($this->httpStatusCode($response), 400);
     }
 }
