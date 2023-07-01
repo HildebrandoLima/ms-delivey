ca34735e4f463d835048d29ebb62de7a7653ae50
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-01 11:51:35 -0300

    Teste de alteração de usuario

--- tests/Feature/User/EditUserTest.php
+++ tests/Feature/User/EditUserTest.php
@@ -3,6 +3,7 @@
 namespace Tests\Feature\User;
 
 use App\Models\User;
+use App\Support\Utils\Enums\PerfilEnum;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -11,19 +12,54 @@ class EditUserTest extends TestCase
     /**
      * @test
      */
-    public function it_endpoint_put_update_a_failure_response(): void
+    public function it_endpoint_put_update_base_response_200(): void
     {
-        //
+        // Arrange
+        $user = User::factory()->createOne()->toArray();
+        $data = [
+            'nome' => $user['name'],
+            'cpf' => '318.794.670-46',
+            'email' => $user['email'],
+            'senha' => 'Password@3',
+            'dataNascimento' => '2023-07-01 14:23:23',
+            'genero' => $user['genero'],
+            'perfil' => false,
+            'ativo' => 0,
+        ];
+        $authenticate = $this->authenticate(PerfilEnum::ADMIN);
+
+        // Act
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $authenticate['accessToken'],
+        ])->putJson(route('user.edit', ['id' => base64_encode($user['id'])]), $data);
+
+        // Assert
+        $this->assertEquals($this->httpStatusCode($response), 200);
     }
 
     /**
      * @test
      */
-    public function it_endpoint_put_update_a_successful_response(): void
+    public function it_endpoint_put_update_base_response_400(): void
     {
-        $user = User::factory()->createOne();
-        $data = $user->toArray();
-        $response = $this->putJson(route('user.edit', ['id' => $data['id']]), $data);
-        $response->assertStatus(200);
+        // Arrange
+        $user = User::factory()->createOne()->toArray();
+        $data = [
+            'nome' => $user['name'],
+            'cpf' => '318.794.670-46',
+            'email' => $user['email'],
+            'dataNascimento' => '2023-07-01 14:23:23',
+            'genero' => $user['genero'],
+            'perfil' => false,
+        ];
+        $authenticate = $this->authenticate(PerfilEnum::ADMIN);
+
+        // Act
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $authenticate['accessToken'],
+        ])->putJson(route('user.edit', ['id' => base64_encode($user['id'])]), $data);
+
+        // Assert
+        $this->assertEquals($this->httpStatusCode($response), 400);
     }
 }
