1aca976af1451ccf4ce66447612009aa2a2786f2
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-02 11:46:44 -0300

    Testes de mudança de senha

---
+++ tests/Feature/Auth/RefreshPasswordTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Feature\Auth;
+
+use App\Models\PasswordReset;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use Illuminate\Support\Str;
+
+class RefreshPasswordTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_endpoint_post_refresh_password_base_response_200(): void
+    {
+        // Arrange
+        $reset = PasswordReset::query()->first()->toArray();
+        $data = [
+            'codigo' => $reset['codigo'],
+            'password' => 'HiLd3br@ndo'
+        ];
+
+        // Act
+        $response = $this->postJson(route('auth.refresh', $reset['token']), $data);
+
+        // Assert
+        $this->assertEquals($this->httpStatusCode($response), 200);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_post_refresh_password_base_response_400(): void
+    {
+        // Arrange
+        $reset = PasswordReset::query()->first()->toArray();
+        $data = [
+            'codigo' => $reset['codigo'],
+            'password' => 'HiLd3br@ndo'
+        ];
+
+        // Act
+        $response = $this->postJson(route('auth.refresh', Str::uuid()), $data);
+
+        // Assert
+        $this->assertEquals($this->httpStatusCode($response), 400);
+    }
+}
