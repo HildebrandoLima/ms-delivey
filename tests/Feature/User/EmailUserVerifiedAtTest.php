ac34a797e91e4a1e49c3214769ab200cd88bfb97
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-03 16:34:45 -0300

    Testes para verificar primeiro acesso de usuários

---
+++ tests/Feature/User/EmailUserVerifiedAtTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Feature\User;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Models\User;
+use Tests\TestCase;
+
+class EmailUserVerifiedAtTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_endpoint_get_base_response_200(): void
+    {
+        // Arrange
+        $data = User::query()->first()->toArray();
+
+        // Act
+        $response = $this->getJson(route('user.email.verified', ['id' => base64_encode($data['id']), 'active' => $data['ativo']]));
+
+        // Assert
+        $this->assertEquals($this->httpStatusCode($response), 200);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_get_base_response_400(): void
+    {
+        // Arrange
+        $data = User::query()->first()->toArray();
+
+        // Act
+        $response = $this->getJson(route('user.email.verified', ['id' => base64_encode($data['id'])]));
+
+        // Assert
+        $this->assertEquals($this->httpStatusCode($response), 400);
+    }
+}
