7ea81f9078625ab5c451ec32d877f1d66cbb6ffc
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-03 15:43:41 -0300

    Testes de listagem de fornecedor

--- tests/Feature/Provider/ListProviderFinderTest.php
+++ tests/Feature/Provider/ListProviderFinderTest.php
@@ -3,27 +3,80 @@
 namespace Tests\Feature\Provider;
 
 use App\Models\Fornecedor;
+use App\Support\Utils\Enums\PerfilEnum;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
+
+use function PHPUnit\Framework\countOf;
 
 class ListProviderFinderTest extends TestCase
 {
     /**
      * @test
      */
-    public function it_endpoint_get_list_find_a_failure_response(): void
+    public function it_endpoint_get_list_find_base_response_200(): void
     {
-        //
+        // Arrange
+        $data = Fornecedor::factory()->createOne()->toArray();
+        $authenticate = $this->authenticate(PerfilEnum::ADMIN);
+
+        // Act
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $authenticate['accessToken'],
+        ])->getJson(route('provider.list.find', ['id' => base64_encode($data['id']), 'active' => 1]));
+
+        // Assert
+        $this->assertEquals($this->httpStatusCode($response), 200);
     }
 
     /**
      * @test
      */
-    public function it_endpoint_get_list_find_a_successful_response(): void
+    public function it_endpoint_get_list_find_base_response_400(): void
     {
-        $provider = Fornecedor::factory()->createOne();
-        $data = $provider->toArray();
-        $response = $this->getJson(route('provider.list.find', ['id' => base64_encode($data['id'])]));
-        $response->assertOk();
+        // Arrange
+        $data = Fornecedor::factory()->createOne()->toArray();
+        $authenticate = $this->authenticate(PerfilEnum::ADMIN);
+
+        // Act
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $authenticate['accessToken'],
+        ])->getJson(route('provider.list.find', ['id' => base64_encode($data['id'])]));
+
+        // Assert
+        $this->assertEquals($this->httpStatusCode($response), 400);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_get_list_find_base_response_401(): void
+    {
+        // Arrange
+        $data = Fornecedor::factory()->createOne()->toArray();
+
+        // Act
+        $response = $this->getJson(route('provider.list.find', ['id' => base64_encode($data['id']), 'active' => 1]));
+
+        // Assert
+        $this->assertEquals($this->httpStatusCode($response), 401);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_get_list_find_base_response_403(): void
+    {
+        // Arrange
+        $data = Fornecedor::factory()->createOne()->toArray();
+        $authenticate = $this->authenticate(PerfilEnum::CLIENTE);
+
+        // Act
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $authenticate['accessToken'],
+        ])->getJson(route('provider.list.find', ['id' => base64_encode($data['id']), 'active' => 1]));
+
+        // Assert
+        $this->assertEquals($this->httpStatusCode($response), 403);
     }
 }
