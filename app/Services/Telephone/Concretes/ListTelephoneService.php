31eeed78c2f9a1478fd1183d0d79efe7900a8111
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-05 17:53:39 -0300

    Teste de listagem de ddd de telefone

--- app/Services/Telephone/Concretes/ListTelephoneService.php
+++ app/Services/Telephone/Concretes/ListTelephoneService.php
@@ -4,11 +4,9 @@ namespace App\Services\Telephone\Concretes;
 
 use App\Repositories\Interfaces\TelephoneRepositoryInterface;
 use App\Services\Telephone\Interfaces\ListTelephoneServiceInterface;
-use App\Support\Permissions\ValidationPermission;
-use App\Support\Utils\Enums\PermissionEnum;
 use Illuminate\Support\Collection;
 
-class ListTelephoneService extends ValidationPermission implements ListTelephoneServiceInterface
+class ListTelephoneService implements ListTelephoneServiceInterface
 {
     private TelephoneRepositoryInterface $telephoneRepository;
 
@@ -19,7 +17,6 @@ class ListTelephoneService extends ValidationPermission implements ListTelephone
 
     public function listDDDAll(): Collection
     {
-        $this->validationPermission(PermissionEnum::LISTAR_DDD);
         return $this->telephoneRepository->getDDDAll();
     }
 }
