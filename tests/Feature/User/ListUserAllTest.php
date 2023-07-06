9678831e50ae05d7929dafac2e06e299a7347f2f
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-06 16:28:48 -0300

    Testes de integração de usuário

--- tests/Feature/User/ListUserAllTest.php
+++ tests/Feature/User/ListUserAllTest.php
@@ -37,7 +37,7 @@ class ListUserAllTest extends TestCase
     public function it_endpoint_get_list_all_base_response_400(): void
     {
         // Arrange
-        User::factory($this->count)->create()->toArray();
+        User::factory($this->count)->make()->toArray();
         $authenticate = $this->authenticate(PerfilEnum::CLIENTE);
 
         // Act
@@ -56,7 +56,7 @@ class ListUserAllTest extends TestCase
     public function it_endpoint_get_list_all_base_response_401(): void
     {
         // Arrange
-        User::factory($this->count)->create()->toArray();
+        User::factory($this->count)->make()->toArray();
 
         // Act
         $response = $this->getJson(route('user.list.all', ['page' => 1, 'perPage' => 10, 'active' => 1]));
