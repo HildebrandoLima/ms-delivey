b91c8d4c21537f255379e9716f69e1ed7837174c
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-06 16:49:00 -0300

    Teste de integração, criação de categoria

--- tests/Feature/Category/CreateCategoryTest.php
+++ tests/Feature/Category/CreateCategoryTest.php
@@ -2,8 +2,9 @@
 
 namespace Tests\Feature\Category;
 
-use App\Models\Categoria;
+use App\Support\Utils\Enums\PerfilEnum;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Support\Str;
 use Tests\TestCase;
 
 class CreateCategoryTest extends TestCase
@@ -11,19 +12,85 @@ class CreateCategoryTest extends TestCase
     /**
      * @test
      */
-    public function it_endpoint_post_create_a_failure_response(): void
+    public function it_endpoint_post_base_response_200(): void
     {
-        //
+        // Arrange
+        $data = [
+            'nome' => Str::random(10),
+            'ativo' => true,
+        ];
+        $authenticate = $this->authenticate(PerfilEnum::ADMIN);
+
+        // Act
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $authenticate['accessToken'],
+        ])->postJson(route('category.save', $data));
+
+        // Assert
+        $this->assertJson($this->baseResponse($response));
+        $this->assertEquals($this->httpStatusCode($response), 200);
     }
 
     /**
      * @test
      */
-    public function it_endpoint_post_create_a_successful_response(): void
+    public function it_endpoint_post_base_response_400(): void
     {
-        $category = Categoria::factory(1)->createOne();
-        $data = $category->toArray();
+        // Arrange
+        $data = [
+            'nome' => Str::random(10),
+            'ativo' => '',
+        ];
+        $authenticate = $this->authenticate(PerfilEnum::ADMIN);
+
+        // Act
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $authenticate['accessToken'],
+        ])->postJson(route('category.save', $data));
+
+        // Assert
+        $this->assertJson($this->baseResponse($response));
+        $this->assertEquals($this->httpStatusCode($response), 400);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_post_base_response_401(): void
+    {
+        // Arrange
+        $data = [
+            'nome' => Str::random(10),
+            'ativo' => true,
+        ];
+
+        // Act
         $response = $this->postJson(route('category.save', $data));
-        $response->assertStatus(200);
+
+        // Assert
+        $this->assertJson($this->baseResponse($response));
+        $this->assertEquals($this->httpStatusCode($response), 401);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_post_base_response_403(): void
+    {
+        // Arrange
+        $data = [
+            'nome' => Str::random(10),
+            'ativo' => true,
+        ];
+        $authenticate = $this->authenticate(PerfilEnum::CLIENTE);
+
+        // Act
+        $response = $this->withHeaders([
+            'Authorization' => 'Bearer '. $authenticate['accessToken'],
+        ])->postJson(route('category.save', $data));
+
+        // Assert
+        $this->assertJson($this->baseResponse($response));
+        $this->assertEquals($this->httpStatusCode($response), 403);
     }
 }
