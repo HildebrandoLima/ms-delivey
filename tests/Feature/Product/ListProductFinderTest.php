3b5d346f7864ce6cf1575c0b9c6aa1d78cf9629a
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-07 16:19:26 -0300

    Testes de integração, listagem de produto

---
+++ tests/Feature/Product/ListProductFinderTest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Tests\Feature\Product;
+
+use App\Models\Produto;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ListProductFinderTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function it_endpoint_get_list_find_base_response_200(): void
+    {
+        // Arrange
+        $data = Produto::factory()->createOne()->toArray();
+
+        // Act
+        $response = $this->getJson(route('product.list.find', ['id' => base64_encode($data['id']), 'active' => 1]));
+
+        // Assert
+        $this->assertJson($this->baseResponse($response));
+        $this->assertEquals($this->httpStatusCode($response), 200);
+    }
+
+    /**
+     * @test
+     */
+    public function it_endpoint_get_list_find_base_response_400(): void
+    {
+        // Arrange
+        $data = Produto::factory()->createOne()->toArray();
+
+        // Act
+        $response = $this->getJson(route('product.list.find', ['id' => base64_encode($data['id'])]));
+
+        // Assert
+        $this->assertJson($this->baseResponse($response));
+        $this->assertEquals($this->httpStatusCode($response), 400);
+    }
+}
