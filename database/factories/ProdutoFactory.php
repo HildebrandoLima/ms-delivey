301038c260a317b02dfe270342c20e5a235bbd36
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-07 12:27:07 -0300

    Testes de integração, cadastro de produto

--- database/factories/ProdutoFactory.php
+++ database/factories/ProdutoFactory.php
@@ -19,16 +19,17 @@ class ProdutoFactory extends Factory
      */
     public function definition()
     {
-        $unidadeMedida = array('UN' => 'UN', 'G' => 'G', 'KG' => 'KG', 'ML' => 'ML', 'L' => 'L', 'M2' => 'M2', 'CX' => 'CX');
+        $unitMeasure = array('UN', 'G', 'KG', 'ML', 'L', 'M2', 'CX');
+        $rand_keys = array_rand($unitMeasure);
         return [
             'nome' => $this->faker->word,
             'preco_custo' => 15.30,
             'margem_lucro' => 4.7,
             'preco_venda' => 20.0,
-            'codigo_barra' => Str::random(13),
+            'codigo_barra' => Str::random_int(13),
             'descricao' => $this->faker->sentence,
             'quantidade' => rand(10, 50),
-            'unidade_medida' => array_rand($unidadeMedida),
+            'unidade_medida' => $unitMeasure[$rand_keys],
             'data_validade' => $this->faker->dateTime,
             'categoria_id' => Categoria::factory()->createOne()->id,
             'fornecedor_id' => Fornecedor::factory()->createOne()->id,
