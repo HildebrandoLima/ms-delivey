be10c50764d53e2111149639ac6ffa4ee2c70269
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-14 19:10:22 -0300

    Testes de unidade, login

--- tests/Unit/Services/Auth/LoginServiceTest.php
+++ tests/Unit/Services/Auth/LoginServiceTest.php
@@ -2,73 +2,40 @@
 
 namespace Tests\Unit\Services\Auth;
 
-use App\Http\Requests\AddressRequest;
-use App\Models\Endereco;
-use App\Models\Fornecedor;
-use App\Models\User;
-use App\Repositories\Interfaces\AddressRepositoryInterface;
-use App\Services\Address\Concretes\CreateAddressService;
-use Illuminate\Support\Str;
+use App\Http\Requests\LoginRequest;
+use App\Repositories\Interfaces\CheckEntityRepositoryInterface;
+use App\Services\Auth\Concretes\LoginService;
+use App\Support\Utils\Enums\PerfilEnum;
 use Mockery\MockInterface;
 use Tests\TestCase;
 
 class LoginServiceTest extends TestCase
 {
-    private AddressRequest $request;
-    private AddressRepositoryInterface $addressRepository;
-    private array $public_place = array('Rua', 'Avenida');
+    private LoginRequest $request;
+    private CheckEntityRepositoryInterface $checkEntityRepository;
 
-    public function test_success_create_address_with_params_user_id_service(): void
+    public function test_success_login_service(): void
     {
         // Arrange
-        $rand_keys = array_rand($this->public_place);
-        $this->request = new AddressRequest();
-        $this->request['logradouro'] = $this->public_place[$rand_keys];
-        $this->request['bairro'] = Str::random(10);
-        $this->request['cidade'] = Str::random(10);
-        $this->request['cep'] = rand(10000, 20000) . '-' . rand(100, 200);
-        $this->request['ufId'] = rand(1, 27);
-        $this->request['usuarioId'] = User::query()->first()->id;
-        $this->request['ativo'] = true;
+        $this->request = new LoginRequest();
+        $this->request['email'] = 'hildebrandolima16@gmail.com';
+        $this->request['password'] = 'HiLd3br@ndo';
 
-        $this->addressRepository = $this->mock(AddressRepositoryInterface::class,
+        $expectedResult = $this->authenticate(PerfilEnum::ADMIN);
+
+        $this->checkEntityRepository = $this->mock(CheckEntityRepositoryInterface::class,
             function (MockInterface $mock) {
-                $mock->shouldReceive('create')->with(Endereco::class)->andReturn(true);
+                $mock->shouldReceive('checkFirstAccess')->with($this->request['email']);
         });
 
         // Act
-        $createAddressService = new CreateAddressService($this->addressRepository);
+        $loginService = new LoginService($this->checkEntityRepository);
 
-        $result = $createAddressService->createAddress($this->request);
-
-        // Assert
-        $this->assertTrue($result);
-    }
-
-    public function test_success_create_address_with_params_provider_id_service(): void
-    {
-        // Arrange
-        $rand_keys = array_rand($this->public_place);
-        $this->request = new AddressRequest();
-        $this->request['logradouro'] = $this->public_place[$rand_keys];
-        $this->request['bairro'] = Str::random(10);
-        $this->request['cidade'] = Str::random(10);
-        $this->request['cep'] = rand(10000, 20000) . '-' . rand(100, 200);
-        $this->request['ufId'] = date('Y-m-d H:i:s');
-        $this->request['fornecedorId'] = Fornecedor::query()->first()->id;
-        $this->request['ativo'] = true;
-
-        $this->addressRepository = $this->mock(AddressRepositoryInterface::class,
-            function (MockInterface $mock) {
-                $mock->shouldReceive('create')->with(Endereco::class)->andReturn(true);
-        });
-
-        // Act
-        $createAddressService = new CreateAddressService($this->addressRepository);
-
-        $result = $createAddressService->createAddress($this->request);
+        $result = $loginService->login($this->request);
 
         // Assert
-        $this->assertTrue($result);
+        $this->assertIsArray($result->toArray());
+        $this->assertIsArray($expectedResult->toArray());
+        $this->assertEquals(count($result->toArray()), count($expectedResult->toArray()));
     }
 }
