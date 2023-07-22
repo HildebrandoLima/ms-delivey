6653502a9dd1952f789a62e93c130f240e7c7521
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-22 15:31:44 -0300

    Testes de unidade, aplicando paginação na camada de serviço

--- tests/Unit/Services/Product/ListProductServiceTest.php
+++ tests/Unit/Services/Product/ListProductServiceTest.php
@@ -7,6 +7,7 @@ use App\Models\Produto;
 use App\Repositories\Interfaces\CheckEntityRepositoryInterface;
 use App\Repositories\Interfaces\ProductRepositoryInterface;
 use App\Services\Product\Concretes\ListProductService;
+use App\Support\Utils\Pagination\Pagination;
 use App\Support\Utils\Pagination\PaginationList;
 use Mockery\MockInterface;
 use Tests\TestCase;
@@ -15,15 +16,20 @@ class ListProductServiceTest extends TestCase
 {
     private CheckEntityRepositoryInterface $checkEntityRepository;
     private ProductRepositoryInterface $productRepository;
+    private Pagination $pagination;
     private int $id;
     private bool $active;
     private string $search;
 
-    public function test_success_list_product_all_service(): void
+    public function test_success_list_product_all_has_paginaiton_service(): void
     {
         // Arrange
         $this->id = rand(1, 100);
         $this->active = true;
+        $this->pagination = new Pagination();
+        $this->pagination['page'] = 1;
+        $this->pagination['perPage'] = 10;
+
         $collection = Produto::query()->with('imagem')->where('produto.ativo', '=', $this->active)->orderByDesc('produto.id')->paginate(10);
         foreach ($collection->items() as $key => $instance):
             $collection[$key] = ProductMapperDto::mapper($instance->toArray());
@@ -37,8 +43,8 @@ class ListProductServiceTest extends TestCase
 
         $this->productRepository = $this->mock(ProductRepositoryInterface::class,
             function (MockInterface $mock) use ($expectedResult) {
-                $mock->shouldReceive('getAll')->with($this->active)
-                ->andReturn($expectedResult);
+                $mock->shouldReceive('getAll')->with($this->pagination, $this->active)
+                     ->andReturn($expectedResult);
         });
 
         // Act
@@ -48,7 +54,49 @@ class ListProductServiceTest extends TestCase
             $this->productRepository
         );
 
-        $result = $listProductServiceTest->listProductAll($this->active);
+        $result = $listProductServiceTest->listProductAll($this->pagination, $this->active);
+
+        // Assert
+        $this->assertSame($result, $expectedResult);
+        $this->assertIsArray($result->toArray()['list']);
+        $this->assertIsArray($expectedResult->toArray()['list']);
+        $this->assertEquals(count($result->toArray()['list']), count($expectedResult->toArray()['list']));
+    }
+
+    public function test_success_list_product_all_no_paginaiton_service(): void
+    {
+        // Arrange
+        $this->id = rand(1, 100);
+        $this->active = true;
+        $this->pagination = new Pagination();
+        $this->pagination['page'] = null;
+        $this->pagination['perPage'] = null;
+
+        $collection = Produto::query()->with('imagem')->where('produto.ativo', '=', $this->active)->orderByDesc('produto.id')->paginate(10);
+        foreach ($collection->items() as $key => $instance):
+            $collection[$key] = ProductMapperDto::mapper($instance->toArray());
+        endforeach;
+        $expectedResult = PaginationList::createFromPagination($collection);
+
+        $this->checkEntityRepository = $this->mock(CheckEntityRepositoryInterface::class,
+            function (MockInterface $mock) {
+                $mock->shouldReceive('checkProductIdExist')->with($this->id);
+        });
+
+        $this->productRepository = $this->mock(ProductRepositoryInterface::class,
+            function (MockInterface $mock) use ($expectedResult) {
+                $mock->shouldReceive('getAll')->with($this->pagination, $this->active)
+                     ->andReturn($expectedResult);
+        });
+
+        // Act
+        $listProductServiceTest = new ListProductService
+        (
+            $this->checkEntityRepository,
+            $this->productRepository
+        );
+
+        $result = $listProductServiceTest->listProductAll($this->pagination, $this->active);
 
         // Assert
         $this->assertSame($result, $expectedResult);
@@ -76,7 +124,7 @@ class ListProductServiceTest extends TestCase
         $this->productRepository = $this->mock(ProductRepositoryInterface::class,
             function (MockInterface $mock) use ($expectedResult) {
                 $mock->shouldReceive('getOne')->with($this->id, $this->search, $this->active)
-                ->andReturn($expectedResult);
+                     ->andReturn($expectedResult);
         });
 
         // Act
@@ -113,7 +161,7 @@ class ListProductServiceTest extends TestCase
         $this->productRepository = $this->mock(ProductRepositoryInterface::class,
             function (MockInterface $mock) use ($expectedResult) {
                 $mock->shouldReceive('getOne')->with($this->id, $this->search, $this->active)
-                ->andReturn($expectedResult);
+                     ->andReturn($expectedResult);
         });
 
         // Act
