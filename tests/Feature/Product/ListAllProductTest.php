085f280c9451e618696ec549d3765563b4200291
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-26 13:23:51 -0300

    Readequação dos testes de integração

--- tests/Feature/Product/ListAllProductTest.php
+++ tests/Feature/Product/ListAllProductTest.php
@@ -50,6 +50,24 @@ class ListAllProductTest extends TestCase
     /**
      * @test
      */
+    public function it_endpoint_get_list_all_as_has_pagination_base_with_search_response_200(): void
+    {
+        // Arrange
+        $data = Produto::factory($this->count)->create()->toArray();
+
+        // Act
+        $response = $this->getJson(route('product.list.all', ['page' => 1, 'perPage' => 10, 'search' => $data[0]['nome'], 'active' => true]));
+
+        // Assert
+        $response->assertOk();
+        $this->assertJson($this->baseResponse($response));
+        $this->assertEquals(count($data), $this->count);
+        $this->assertEquals($this->httpStatusCode($response), 200);
+    }
+
+    /**
+     * @test
+     */
     public function it_endpoint_get_list_all_base_response_400(): void
     {
         // Arrange
