8febd738d481df1a17b887228439849748772c73
HildebrandoLima <hildebrandolima16@gmail.com>
2023-07-27 09:25:16 -0300

    Testes de unidade

--- tests/Unit/Services/Provider/EditProviderServiceTest.php
+++ tests/Unit/Services/Provider/EditProviderServiceTest.php
@@ -2,9 +2,8 @@
 
 namespace Tests\Unit\Services\Provider;
 
-use App\Http\Requests\ProviderRequest;
+use App\Http\Requests\Provider\EditProviderRequest;
 use App\Models\Fornecedor;
-use App\Repositories\Interfaces\CheckEntityRepositoryInterface;
 use App\Repositories\Interfaces\ProviderRepositoryInterface;
 use App\Services\Provider\Concretes\EditProviderService;
 use App\Support\Generate\GenerateCNPJ;
@@ -16,16 +15,14 @@ use Tests\TestCase;
 
 class EditProviderServiceTest extends TestCase
 {
-    private ProviderRequest $request;
-    private CheckEntityRepositoryInterface $checkEntityRepository;
+    private EditProviderRequest $request;
     private ProviderRepositoryInterface $providerRepository;
-    private int $id;
 
     public function test_success_edit_user_service(): void
     {
         // Arrange
-        $this->id = rand(1, 100);
-        $this->request = new ProviderRequest();
+        $this->request = new EditProviderRequest();
+        $this->request['id'] = rand(1, 100);
         $this->request['razaoSocial'] = Str::random(10);
         $this->request['cnpj'] = GenerateCNPJ::generateCNPJ();
         $this->request['email'] = GenerateEmail::generateEmail();
@@ -38,25 +35,16 @@ class EditProviderServiceTest extends TestCase
             'Authorization' => 'Bearer '. $authenticate['accessToken'],
         ]);
 
-        $this->checkEntityRepository = $this->mock(CheckEntityRepositoryInterface::class,
-            function (MockInterface $mock) {
-                $mock->shouldReceive('checkProviderIdExist')->with($this->id);
-        });
-
         $this->providerRepository = $this->mock(ProviderRepositoryInterface::class,
             function (MockInterface $mock) {
-                $mock->shouldReceive('update')->with($this->id, Fornecedor::class)
+                $mock->shouldReceive('update')->with(Fornecedor::class)
                      ->andReturn(true);
         });
 
         // Act
-        $editProviderService = new EditProviderService
-        (
-            $this->checkEntityRepository,
-            $this->providerRepository
-        );
+        $editProviderService = new EditProviderService($this->providerRepository);
 
-        $result = $editProviderService->editProvider($this->id, $this->request);
+        $result = $editProviderService->editProvider($this->request);
 
         // Assert
         $this->assertTrue($result);
