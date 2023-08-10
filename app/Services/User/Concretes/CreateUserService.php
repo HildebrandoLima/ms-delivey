1c147d35b3179a65bbb7932d8e4f4aba60a2bb65
Hildebrando Alves <hildebrandolima16@gmail.com>
2023-08-10 16:11:01 -0300

    Titrado permissão de cliente

--- app/Services/User/Concretes/CreateUserService.php
+++ app/Services/User/Concretes/CreateUserService.php
@@ -16,7 +16,7 @@ class CreateUserService implements CreateUserServiceInterface
     private IEntityRepository $userRepository;
     private IPermissionRepository $permissionRepository;
     private array $permissionsAdmin = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19];
-    private array $permissionsClient = [3, 4, 7, 10, 11, 14, 15, 18, 19];
+    private array $permissionsClient = [3, 4, 7, 10, 11, 14, 18, 19];
 
     public function __construct
     (
