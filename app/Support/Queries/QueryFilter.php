cb6578491344de662e9dbfddfeacdc0e899405ed
Hildebrando Alves <hildebrandolima16@gmail.com>
2023-08-12 12:10:37 -0300

    Final class para classe de querie ativo

--- app/Support/Queries/QueryFilter.php
+++ app/Support/Queries/QueryFilter.php
@@ -5,7 +5,7 @@ namespace App\Support\Queries;
 use App\Support\Enums\AtivoEnum;
 use Illuminate\Database\Eloquent\Builder;
 
-class QueryFilter
+final class QueryFilter
 {
     final public static function getQueryFilter(Builder $query, bool $filter)
     {
