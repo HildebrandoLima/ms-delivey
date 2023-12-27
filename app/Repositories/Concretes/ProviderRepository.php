1ecb4e8c061998a8d1a453c5e738d532f54b3ccd
Hildebrando Alves <hildebrandolima16@gmail.com>
2023-12-27 14:41:18 -0300

    Adicionado paginação para listagem de fornecedores

--- app/Repositories/Concretes/ProviderRepository.php
+++ app/Repositories/Concretes/ProviderRepository.php
@@ -7,26 +7,20 @@ use App\Models\Fornecedor;
 use App\Repositories\Abstracts\IProviderRepository;
 use App\Support\AutoMapper\AutoMapper;
 use App\Support\Queries\QueryFilter;
+use App\Support\Utils\Pagination\Pagination;
 use App\Support\Utils\Pagination\PaginationList;
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Support\Collection;
 
 class ProviderRepository implements IProviderRepository
 {
-    public function readAll(string $search, bool $filter): Collection
+    public function readAll(Pagination $pagination, string $search, bool $filter): Collection
     {
-        $collection = Fornecedor::with('endereco')->with('telefone')
-        ->where(function($query) use ($search, $filter) {
-            QueryFilter::getQueryFilter($query, $filter);
-            if (!empty($search)):
-                $query->where('fornecedor.razao_social', 'like', $search);
-            else:
-                QueryFilter::getQueryFilter($query, $filter);
-            endif;
-        })->orderByDesc('fornecedor.id')->paginate(10);
-        foreach ($collection->items() as $key => $instance):
-            $collection[$key] = $this->map($instance->toArray());
-        endforeach;
-        return PaginationList::createFromPagination($collection);
+        if (isset($pagination->page) && isset($pagination->perPage)):
+            return $this->hasPagination($search, $filter);
+        else:
+            return $this->noPagination($search, $filter);
+        endif;
     }
 
     public function readOne(int $id, bool $filter): Collection
@@ -39,6 +33,37 @@ class ProviderRepository implements IProviderRepository
             $collection[$key] = $this->map($instance);
         endforeach;
         return $collection;
+    }
+
+    private function hasPagination(string $search, bool $filter): Collection
+    {
+        $collection = $this->query($search, $filter)->paginate(10);
+        foreach ($collection->items() as $key => $instance):
+          $collection[$key] = $this->map($instance->toArray());
+        endforeach;
+        return PaginationList::createFromPagination($collection);
+    }
+
+    private function noPagination(string $search, bool $filter): Collection
+    {
+        $collection = $this->query($search, $filter)->get();
+        foreach ($collection->toArray() as $key => $instance):
+            $collection[$key] = $this->map($instance);
+        endforeach;
+        return $collection;
+    }
+
+    private function query(string $search, bool $filter): Builder
+    {
+        return Fornecedor::with('endereco')->with('telefone')
+        ->where(function($query) use ($search, $filter) {
+            QueryFilter::getQueryFilter($query, $filter);
+            if (!empty($search)):
+                $query->where('fornecedor.razao_social', 'like', $search);
+            else:
+                QueryFilter::getQueryFilter($query, $filter);
+            endif;
+        })->orderByDesc('id');
     }
 
     private function map(array $data): ProviderDto
