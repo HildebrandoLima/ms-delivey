70bc42b65194a478b00760f05c1356be7eab2a01
Hildebrando Alves <hildebrandolima16@gmail.com>
2023-12-29 16:11:10 -0300

    Teste de uniade

--- tests/Unit/Services/User/ListUserServiceTest.php
+++ tests/Unit/Services/User/ListUserServiceTest.php
@@ -5,19 +5,22 @@ namespace Tests\Unit\Services\User;
 use App\Repositories\Abstracts\IUserRepository;
 use App\Services\User\Concretes\ListUserService;
 use App\Support\Enums\PerfilEnum;
+use App\Support\Utils\Pagination\Pagination;
 use Mockery\MockInterface;
 use Tests\TestCase;
 
 class ListUserServiceTest extends TestCase
 {
     private IUserRepository $userRepository;
+    private Pagination $pagination;
     private int $id;
     private bool $filter;
     private string $search;
 
-    public function test_success_list_user_all_service(): void
+    public function test_success_list_user_all_has_pagination_service(): void
     {
         // Arrange
+        $this->pagination = new Pagination();
         $this->search = '';
         $this->filter = true;
 
@@ -30,14 +33,43 @@ class ListUserServiceTest extends TestCase
 
         $this->userRepository = $this->mock(IUserRepository::class,
             function (MockInterface $mock) use ($expectedResult) {
-                $mock->shouldReceive('readAll')->with($this->search, $this->filter)
+                $mock->shouldReceive('readAll')->with(Pagination::class, $this->search, $this->filter)
                      ->andReturn($expectedResult);
         });
 
         // Act
         $listUserService = new ListUserService($this->userRepository);
 
-        $result = $listUserService->listUserAll($this->search, $this->filter);
+        $result = $listUserService->listUserAll($this->pagination, $this->search, $this->filter);
+
+        // Assert
+        $this->assertSame($result, $expectedResult);
+    }
+
+    public function test_success_list_user_all_no_pagination_service(): void
+    {
+        // Arrange
+        $this->pagination = new Pagination();
+        $this->search = '';
+        $this->filter = true;
+
+        $expectedResult = $this->paginationList();
+
+        $authenticate = $this->authenticate(PerfilEnum::ADMIN);
+        $this->withHeaders([
+            'Authorization' => 'Bearer '. $authenticate['accessToken'],
+        ]);
+
+        $this->userRepository = $this->mock(IUserRepository::class,
+            function (MockInterface $mock) use ($expectedResult) {
+                $mock->shouldReceive('readAll')->with(Pagination::class, $this->search, $this->filter)
+                     ->andReturn($expectedResult);
+        });
+
+        // Act
+        $listUserService = new ListUserService($this->userRepository);
+
+        $result = $listUserService->listUserAll($this->pagination, $this->search, $this->filter);
 
         // Assert
         $this->assertSame($result, $expectedResult);
