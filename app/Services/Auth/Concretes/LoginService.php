92617fa73a20cf4db419f8920e377a9f56fbd4bd
Hildebrando Alves <hildebrandolima16@gmail.com>
2024-03-14 13:17:40 -0300

    Response error nas classes de login e logout

--- app/Services/Auth/Concretes/LoginService.php
+++ app/Services/Auth/Concretes/LoginService.php
@@ -2,22 +2,30 @@
 
 namespace App\Services\Auth\Concretes;
 
+use App\Exceptions\BaseResponseError;
 use App\Http\Requests\Auth\LoginRequest;
 use App\Services\Auth\Abstracts\ILoginService;
 use App\Support\Enums\PerfilEnum;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Illuminate\Support\Collection;
 
 class LoginService implements ILoginService
 {
     public function login(LoginRequest $request): Collection
     {
-        return collect([
-            'accessToken' => auth()->attempt($request->only(['email', 'password'])),
-            'userId' => auth()->user()->id,
-            'userName' => auth()->user()->nome,
-            'userEmail' => auth()->user()->email,
-            'isAdmin' => auth()->user()->e_admin == 1 ? (bool)PerfilEnum::ADMIN : (bool)PerfilEnum::CLIENTE,
-            'permissions' => auth()->user()->permissions,
-        ]);
+        $auth = auth()->attempt($request->only(['email', 'password']));
+        if ($auth):
+            $user = auth()->user();
+            return collect([
+                'accessToken' => $auth,
+                'userId' => $user->id,
+                'userName' => $user->nome,
+                'userEmail' => $user->email,
+                'isAdmin' => $user->e_admin == 1 ? (bool)PerfilEnum::ADMIN : (bool)PerfilEnum::CLIENTE,
+                'permissions' => $user->permissions,
+            ]);
+        else:
+            throw new HttpResponseException(BaseResponseError::httpBadRequest(collect(), collect()));
+        endif;
     }
 }
