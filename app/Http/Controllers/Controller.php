d3a86e820dda203cb788a6ba996648008a0d5ad0
HildebrandoLima <hildebrandolima16@gmail.com>
2024-04-12 14:18:06 -0300

    Refatorado controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,19 +2,15 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
-use Illuminate\Foundation\Bus\DispatchesJobs;
-use Illuminate\Foundation\Validation\ValidatesRequests;
+use App\Exceptions\HttpInternalServerError;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Collection;
 use Symfony\Component\HttpFoundation\Response;
-use Illuminate\Support\Facades\Log;
+use Illuminate\Http\Exceptions\HttpResponseException;
 use Exception;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
-
     public function get(Collection $success): Response
     {
         return response()->json([
@@ -57,12 +53,6 @@ class Controller extends BaseController
 
     public function error(Exception $details): Response
     {
-        Log::error("Error: [" . $details->getMessage() . "]");
-        return response()->json([
-            "message" => "Error ao efetuar ação.",
-            "data" => [],
-            "status" => Response::HTTP_BAD_REQUEST,
-            "details" => $details->getMessage(),
-        ], Response::HTTP_BAD_REQUEST);
+        throw new HttpResponseException(HttpInternalServerError::getResponse($details));
     }
 }
