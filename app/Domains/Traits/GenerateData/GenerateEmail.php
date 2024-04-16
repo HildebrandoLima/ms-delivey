93d3368e7cfa2df9a2134dae49396b7a71dbd806
HildebrandoLima <hildebrandolima16@gmail.com>
2024-04-16 10:17:16 -0300

    Refatorado @

--- app/Domains/Traits/GenerateData/GenerateEmail.php
+++ app/Domains/Traits/GenerateData/GenerateEmail.php
@@ -8,9 +8,9 @@ trait GenerateEmail
 {
     public function generateEmail(): string
     {
-        $dominio = array('@gmail.com', '@outlook.com', '@business.com.br');
+        $dominio = array('gmail.com', 'outlook.com', 'business.com.br');
         $rand_keys = array_rand($dominio);
-        $email = Str::random(10) . $dominio[$rand_keys];
+        $email = Str::random(10) . '@' . $dominio[$rand_keys];
         return $email;
     }
 }
